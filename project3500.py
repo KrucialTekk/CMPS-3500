28a709bcf4792d38aae35b7a7dc576d24db066a7
Noah <128880146+nhmllx@users.noreply.github.com>
2023-03-28 19:46:48 -0700

    Update project3500.py

--- project3500.py
+++ project3500.py
@@ -8,19 +8,29 @@
 # To compile and run python from odin, type 'python3 project3500.py' or './project3500.py'
 
 import pandas as pd #library to search, sort, parse the csv file
+import datetime
 
-#Various tests
-hello = 'hi guys'
-myList = [1,2,3]
-print(hello)
-print(myList[1])
+#Main menu++++++++++++++++++++++++++++++++++++++++
+print("Main Menu:\n*********")
+select = input("(1) Load Data\n(2) Exploring Data\n(3) Data Analysis\n(4) Print Data Set\n(5) Quit\n")
+while select.isnumeric() != 1:#Error check is input is not a number
+    print("-Input was not valid. Try again.-")
+    select = input("(1) Load Data\n(2) Exploring Data\n(3) Data Analysis\n(4) Print Data Set\n(5) Quit\n")
 
-data_frame = pd.read_csv('Crime_Data_from_2017_to_2019.csv')#read the csv
-print(data_frame.head(5))#this prints 5 lines of data_frame
-
-list2 = ["LAT", "LON"]
-#example of grouping the csv file for something specific
-location = data_frame.groupby(["LOCATION"], as_index=False)[list2].count()
-print(location.head(10))
-
-
+while True:
+    if (select == "1"):
+        data_frame = pd.read_csv('Crime_Data_from_2017_to_2019.csv')#read the csv
+        if data_frame.empty != True:
+            print("File loaded successfully!")
+    elif (select == "2"):
+        #example of grouping the csv file for something specific
+        list2 = ["LAT", "LON"]
+        location = data_frame.groupby(["LOCATION"], as_index=False)[list2].count()
+        print(location.head(10))
+    #elif (select == "3"): #Data analysis called here
+    elif (select == "4"):
+        print(data_frame.head(5))#this prints 5 lines of data_frame
+    elif (select == "5"):
+        exit(0)
+    print("\n-Input another valid command-")
+    select = input("(1) Load Data\n(2) Exploring Data\n(3) Data Analysis\n(4) Print Data Set\n(5) Quit\n")
