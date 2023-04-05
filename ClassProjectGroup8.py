9f1da8ff6d7d470c50f579eb2e709e623e67ff0d
Andrew Nguyen <61101269+andrewnguyen9029@users.noreply.github.com>
2023-04-04 19:29:17 -0700

    Spacing and comments

--- ClassProjectGroup8.py
+++ ClassProjectGroup8.py
@@ -13,10 +13,19 @@ import numpy
 import sys
 import traceback
 import logging
-import csv #read the csv file(s)
-import time #fine the load time or time to load 
+import csv # read the csv file(s)
+import time # fine the load time or time to load 
 
-selectedfile = 'Crime_Data_from_2017_to_2019.csv'#This variable is used to hold the correct file name. Set to the first csv file by default, but can be changed to the other csv files in main menu option 1
+try:
+    data_frame = pd.read_csv('Crime_Data_from_2017_to_2019.csv')# read the csv (move these from local to global)
+except OSError as e:
+    print("File doesn't exsist: Catching Error")
+    print(f"{type(e)}: {e}")
+    exit(0)
+     
+current_time = datetime.datetime.now()# [current time] 'YYYY-MM-DD HH:MM:SS.ssssss'
+formatted_time = current_time.strftime('%H:%M:%S') # [current time] HH:MM:SS
+selectedfile = 'Crime_Data_from_2017_to_2019.csv'# This variable is used to hold the correct file name. Set to the first csv file by default, but can be changed to the other csv files in main menu option 1
 
 # Function Code Here ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -24,7 +33,7 @@ def testFunc(num1, num2):
     sum = num1 + num2
     return sum
 
-def counter(): # this doesnt do anything yet
+def counter(): #this doesnt do anything yet
     return 0
 
 # Main Menu++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -38,9 +47,6 @@ while select.isnumeric() != 1:# Error check is input is not a number
 while True:
     try:
         if (select == "1"):
-        #data_frame = pd.read_csv('Crime_Data_from_2017_to_2019.csv')#read the csv
-        #current_time = datetime.datetime.now()#[current time] 'YYYY-MM-DD HH:MM:SS.ssssss'
-        #formatted_time = current_time.strftime('%H:%M:%S') #[current time] HH:MM:SS
             if data_frame.empty != True:
                 print("Load data set:\n**************")
                 print(formatted_time,"Select the numer of the file to load from the list below:")
@@ -48,10 +54,10 @@ while True:
                 select_file = input(" Please select a file: \n[1] Crime_Data_from_2017_to_2019.csv\n[2] Crime_Data_from_2020_to_2021.csv\n[3] Test.csv\n")
             if(select_file == "1"):
                 print(formatted_time,"You selected: Option 1")
-                selectedfile = 'Crime_Data_from_2017_to_2019.csv'# use this to read the correct file whenever it is needed
+                selectedfile = 'Crime_Data_from_2017_to_2019.csv'#use this to read the correct file whenever it is needed
                 start_time = time.time()
                 # with open('Crime_Data_from_2017_to_2019.csv', 'r') as file: #open and close file
-                with open(selectedfile, 'r') as file: #open and close file
+                with open(selectedfile, 'r') as file: # open and close file
                     reader = csv.reader(file) # obj from the csv file
                     header = next(reader) # get the row
                     col = len(header) # count the number of colums in the row
@@ -81,7 +87,7 @@ while True:
             select_2 = input("(21) List all columns:\n(22) Drop Columns:\n(23) Describe Columns:\n(24) Search Element in Column:\n(25) Back to Main Menu\n")
             if (select_2 == "21"):
                 print('(21) List all columns: \n******************')
-                with open('Crime_Data_from_2017_to_2019.csv', 'r') as file: #open and close file
+                with open('Crime_Data_from_2017_to_2019.csv', 'r') as file: # open and close file
                     reader = csv.reader(file) # obj from the csv file
                     columns = next(reader) # get the row
                     for i, column in enumerate(columns):
@@ -117,11 +123,11 @@ while True:
                 print('(24) Search Element in Column: \n******************')
                 print(formatted_time, 'Select column number to perform a search:') 
 
-                with open('Crime_Data_from_2017_to_2019.csv', 'r') as file: #open and close file and display the columns 
-                    reader = csv.reader(file) #obj from the csv file
-                    columns = next(reader) #get the row
+                with open('Crime_Data_from_2017_to_2019.csv', 'r') as file: # open and close file and display the columns 
+                    reader = csv.reader(file) # obj from the csv file
+                    columns = next(reader) # get the row
                     for i, column in enumerate(columns):
-                        print(f"[{1+i}] <{column} >") #print the index of the columns 
+                        print(f"[{1+i}] <{column} >") # print the index of the columns 
 
                 selectCol = int(input(""))
                 print(formatted_time, "You selected column: ", selectCol)
@@ -129,39 +135,40 @@ while True:
                 selectElement = int(input(""))
             
                 data_frame.loc[selectElement]
-               #help me
-                #rows = []
-                #with open('Crime_Data_from_2017_to_2019.csv', 'r') as file_in, open('new_Crime_Data_from_2017_to_2019.csv', 'w', newline='' ) as file_out:
-                #   reader = csv.reader(file_in) #read to csv file 
-                #  writer = csv.writer(file_out) #write to csv file 
-                # for row in reader:
-                    # Ret = row[selectCol]
-                        #if row == selectElement:
-                        #   myRet = selectElement
-                        #  print(formatted_time, row)
-                        # else :
-                            # myRet = 0
-                        # print("Element not found in Column: ", selectCol)
+                # help me
+                # rows = []
+                # with open('Crime_Data_from_2017_to_2019.csv', 'r') as file_in, open('new_Crime_Data_from_2017_to_2019.csv', 'w', newline='' ) as file_out:
+                    # reader = csv.reader(file_in) #read to csv file 
+                    # writer = csv.writer(file_out) #write to csv file 
+                    # for row in reader:
+                        # Ret = row[selectCol]
+                        # if row == selectElement:
+                            # myRet = selectElement
+                            # print(formatted_time, row)
+                    # else :
+                        # myRet = 0
+                         # print("Element not found in Column: ", selectCol)
 
-            #    if myRet == 0:
-            #    print("Element not found in Column: ", selectCol)
-            # else :
+                # if myRet == 0:
+                # print("Element not found in Column: ", selectCol)
+           # else :
                 print(formatted_time)
 
             if (select_2 == "25"):
                 print('(25)Back to Main Menu: \n******************') # Make it when the user selecet 25, it goes back to main
-             #   select = input("(1) Load Data\n(2) Exploring Data\n(3) Data Analysis\n(4) Print Data Set\n(5) Quit\n")
+                # select = input("(1) Load Data\n(2) Exploring Data\n(3) Data Analysis\n(4) Print Data Set\n(5) Quit\n")
 
         # example of grouping the csv file for something specific
         # list2 = ["LAT", "LON"]
         # location = data_frame.groupby(["LOCATION"], as_index=False)[list2].count()
         # print(location.head(10))
-    #elif (select == "3"): #Data analysis called here
+
+        # elif (select == "3"): # Data analysis called here
         elif (select == "4"):
-            print(data_frame.head(5))#this prints 5 lines of data_frame
+            print(data_frame.head(5)) # this prints 5 lines of data_frame
         elif (select == "5"):
             exit(0)
-        # else :
+        #else :
         print("\n-Input another valid command-")
         select = input("(1) Load Data\n(2) Exploring Data\n(3) Data Analysis\n(4) Print Data Set\n(5) Quit\n")
     except Exception as err:
