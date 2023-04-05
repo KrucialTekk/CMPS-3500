dabcab8a59ce2585b84579dba44383844056e51d
Andrew Nguyen <61101269+andrewnguyen9029@users.noreply.github.com>
2023-04-04 20:32:19 -0700

    count - max (all the functions) print
    print only

--- ClassProjectGroup8.py
+++ ClassProjectGroup8.py
@@ -140,6 +140,17 @@ while True:
                 print("Column", selected_column, "stats: " )
                 print("=========")
                 print("Count: ",element_count)
+                print("Unqiue: ")
+                print("Mean: ")
+
+                print("Median: ")
+                print("Mode: ")
+
+                print("Standard Deviation: ")
+                print("Variance: ")
+
+                print("Minimum: ")
+                print("Maximum: ")
 
                 # Call the count_elements_in_selected_column function to count the elements in the selected column
                 column_index = selected_column - 1  # Adjust index since user input is 1-based and Python list indexing is 0-based
